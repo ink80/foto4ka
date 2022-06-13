a891575a7060071796194882851b46e1e49a6c10
ink80 <igorkozachuk@gmail.com>
2022-06-13 08:41:18 +0300

    change config.sign_out_via to get, fix user signout process

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  validates :user_id, presence: true
 
   belongs_to :user
 end
