738621a9e971397971747e0f34e194855c27f7d8
ink80 <igorkozachuk@gmail.com>
2022-06-14 12:55:15 +0300

    Coments added to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_070826) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_095057) do
+  create_table "comments", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -41,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_070826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
