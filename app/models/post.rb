fbb3bb0ea3e4fba62953e1f18b752bd61f8510a8
ink80 <igorkozachuk@gmail.com>
2022-06-21 15:25:24 +0300

    visual fixes for post templates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 class Post < ApplicationRecord
   attr_accessor :image
 
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, 
+  has_attached_file :image, styles: { big: '500x500>', medium: '300x300>', thumb: '100x100>' },
                             escape_url: false,
                             url: '/assets/posts/:id/:style/:basename.:extension',
                             path: ':rails_root/public/assets/posts/:id/:style/:basename.:extension'
-  
-                            has_many :comments, dependent: :destroy
+
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment :image, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
 
   belongs_to :user
 end
