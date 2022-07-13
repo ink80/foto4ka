3c7e93e4c4ff68c133e237360f29a080dc768acd
ink80 <igorkozachuk@gmail.com>
2022-07-13 16:34:37 +0300

    added following posts to user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
   enum role: %i[user vip admin], _suffix: :role
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :follower_relationships,
+           foreign_key: :following_id,
+           class_name: 'Follow'
+
   has_many :followers, through: :follower_relationships, source: :follower
 
-  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following_relationships,
+           foreign_key: :follower_id,
+           class_name: 'Follow'
+
   has_many :following, through: :following_relationships, source: :following
 
   devise :database_authenticatable, :registerable,
@@ -17,4 +22,16 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100#' },
                              default_url: '/images/:style/missing.png'
   validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\Z}
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
+
+  def following?(user_id)
+    following.include?(user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
+  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users
   get 'persons/profile', as: 'user_root'
 
   root 'posts#index'
-  
+
   resources :posts do
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: %i[create destroy]
     resources :comments
   end
+
+  resources :users do
+    resources :posts
+    member do
+      get :following, :followers
+    end
+  end
+
 end
