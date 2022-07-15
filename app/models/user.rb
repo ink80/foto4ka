aa9c07d19dc700b1a594f27a29237aff778570a4
ink80 <igorkozachuk@gmail.com>
2022-07-15 09:56:26 +0300

    acts_as_follower gem added

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+    
+  acts_as_followable
+  acts_as_follower
+
 end
