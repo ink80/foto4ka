7cfb1664a44d224f6c6754606d2b3dc2d9e50ad4
ink80 <igorkozachuk@gmail.com>
2022-07-19 19:07:55 +0300

    fix image migration

--- db/migrate/20220615072054_add_image_to_post.rb
+++ db/migrate/20220615072054_add_image_to_post.rb
@@ -1,15 +1,15 @@
 class AddImageToPost < ActiveRecord::Migration[7.0]
   def up
     add_column :posts, :image_file_name, :string
-    add_column :posts, :image__file_size, :integer
-    add_column :posts, :image__content_type, :string
-    add_column :posts, :image__updated_at, :datetime
+    add_column :posts, :image_file_size, :integer
+    add_column :posts, :image_content_type, :string
+    add_column :posts, :image_updated_at, :datetime
   end
 
   def down
-    remove_column :posts, :image__file_name, :string
-    remove_column :posts, :image__file_size, :integer
-    remove_column :posts, :image__content_type, :string
-    remove_column :posts, :image__updated_at, :datetime
+    remove_column :posts, :image_file_name, :string
+    remove_column :posts, :image_file_size, :integer
+    remove_column :posts, :image_content_type, :string
+    remove_column :posts, :image_updated_at, :datetime
   end
 end
