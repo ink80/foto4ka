381bdf32cec9d05473d764aaa5644064ed628d42
ink80 <igorkozachuk@gmail.com>
2022-07-19 18:04:01 +0300

    change migration for image post

--- db/migrate/20220615072054_add_image_to_post.rb
+++ db/migrate/20220615072054_add_image_to_post.rb
@@ -1,11 +1,15 @@
 class AddImageToPost < ActiveRecord::Migration[7.0]
-  def self.up
-    change_table :posts do |t|
-      t.attachment :image
-    end
+  def up
+    add_column :posts, :image_file_name, :string
+    add_column :posts, :image__file_size, :integer
+    add_column :posts, :image__content_type, :string
+    add_column :posts, :image__updated_at, :datetime
   end
 
-  def self.down
-    remove_attachment :posts, :image
+  def down
+    remove_column :posts, :image__file_name, :string
+    remove_column :posts, :image__file_size, :integer
+    remove_column :posts, :image__content_type, :string
+    remove_column :posts, :image__updated_at, :datetime
   end
 end
